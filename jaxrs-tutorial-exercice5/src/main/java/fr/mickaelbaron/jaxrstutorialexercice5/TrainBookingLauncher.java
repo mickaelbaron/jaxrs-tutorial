2d99c8f9804fe7df74ab0b500b17b45f7f4c0cd7
Mickael BARON <baron@ensma.fr>
2022-04-22 18:30:18 +0200

    Migrate to Jakarta version

--- jaxrs-tutorial-exercice5/src/main/java/fr/mickaelbaron/jaxrstutorialexercice5/TrainBookingLauncher.java
+++ jaxrs-tutorial-exercice5/src/main/java/fr/mickaelbaron/jaxrstutorialexercice5/TrainBookingLauncher.java
@@ -3,12 +3,12 @@ package fr.mickaelbaron.jaxrstutorialexercice5;
 import java.net.URI;
 import java.util.logging.Level;
 
-import javax.ws.rs.core.UriBuilder;
-
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
+
+import jakarta.ws.rs.core.UriBuilder;
 
 /**
  * @author Mickael BARON (baron.mickael@gmail.com)
@@ -25,14 +25,13 @@ public class TrainBookingLauncher {
 		ResourceConfig rc = new ResourceConfig();
 		rc.registerClasses(TrainResource.class);
 		rc.property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_SERVER, Level.WARNING.getName());
-		
+
 		try {
 			HttpServer server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
 			server.start();
 
 			System.out.println(String.format(
-					"Jersey app started with WADL available at " + "%s\nHit enter to stop it...",
-					BASE_URI, BASE_URI));
+					"Jersey app started with WADL available at " + "%s\nHit enter to stop it...", BASE_URI, BASE_URI));
 
 			System.in.read();
 			server.shutdownNow();
